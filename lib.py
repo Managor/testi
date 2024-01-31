389cbd035c6d8b0ff009c5ca39d51d0fe7376744
Antti Savolainen <antti.savo@gmail.com>
2024-01-31 17:43:52 +0200

    Virheellinen commit viesti, plz vaihda

--- lib.py
+++ lib.py
@@ -1,3 +1,9 @@
 class lib:
   def __init__(self) -> None:
     print("Paska")
+
+  def kertotaulu(self) -> None:
+    for x in range(1,11):
+      for y in range(1,11):
+        print(f"{x*y:4}", end="")
+      print()
